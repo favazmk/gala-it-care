5fdf0bdea83ea04ae0d0f4bbcf22d9a3abd8fedf
favazmk12-creator <favazmk12@gmail.com>
2026-08-31 14:04:18 +0530

    chore: add google tag manager tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID || 'GTM-XXXXXXX'}');
+            })(window,document,'script','dataLayer','GTM-P7XD7GKJ');
           `}
         </Script>
       </head>
@@ -50,7 +50,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID || 'GTM-XXXXXXX'}`}
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P7XD7GKJ"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
